cded325a50e37de58596372e90ae0b6b91febc86
Aras <vahitaras@gmail.com>
2023-12-15 06:51:28 -0500

    finish

--- README.md
+++ README.md
@@ -24,3 +24,15 @@
         "content": "This is my content."
     }
     ```
+
+- This section prevents CORS error from another website
+    ```javascript
+    // CORS Error Prevention
+    app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+    next();
+    });
+    ```
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 
+const feedRoutes = require("./routes/feedRoutes");
+
+// CORS Error Prevention
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  next();
+});
+
 // REST API deals with JSON data, don't use it.
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json({ limit: "50mb" }));
-
-const feedRoutes = require("./routes/feedRoutes");
 
 app.use("/feed", feedRoutes);
 
